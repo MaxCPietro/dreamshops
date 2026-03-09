465190d6b75635d26292347bbf3963e022e2d864
MaxCPietro <maxcorrea81@outlook.com>
2026-03-09 18:27:29 -0300

    Hasta modelado completo

--- src/main/java/com/waipara/dreamshops/model/Category.java
+++ src/main/java/com/waipara/dreamshops/model/Category.java
@@ -1,9 +1,24 @@
 package com.waipara.dreamshops.model;
 
-import java.util.List;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.List;
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @OneToMany(mappedBy = "category")
     private List<Product> products;
 }
--- src/main/java/com/waipara/dreamshops/model/Image.java
+++ src/main/java/com/waipara/dreamshops/model/Image.java
@@ -1,4 +1,30 @@
 package com.waipara.dreamshops.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Blob;
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
 public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fileName;
+    private String fileType;
+
+    @Lob
+    private Blob image;
+    private String donwloadUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
 }
